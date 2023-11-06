b0419f764c31caca0322ea14a9b5deffa0909fa3
Florin <florinfire80@yahoo.com>
2023-11-06 08:28:31 +0200

    commit ex 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Profile from './Profile';
+import Statistics from './Statistics';
 
 export const App = () => {
   return (
     <main>
       <Profile />
+      <Statistics />
     </main>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import data from './data.json';
+import styles from './Statistics.module.css';
+
+const Statistics = ({ title }) => {
+  const [randomColor, setRandomColor] = useState(generateRandomColor());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setRandomColor(generateRandomColor());
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  function generateRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  return (
+    <section className={styles.statistics}>
+      <h2 className={styles.statisticsTitle}>Upload stats</h2>
+
+      {/* {title && <h2 className={styles.statisticsTitle}>{title}</h2>} */}
+
+      <ul className={styles.statisticsStatList}>
+        {data.map(item => (
+          <li
+            key={item.id}
+            className={styles.statisticsStatItem}
+            style={{ backgroundColor: generateRandomColor() }}
+          >
+            <span className={styles.statisticsStatLabel}>{item.label}</span>
+            <br />
+            <span className={styles.statisticsStatPercentage}>
+              {item.percentage}%
+            </span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Statistics;
